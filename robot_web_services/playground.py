25cbb22a9e517b1ce95bed655e6320e83727336e
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-22 22:18:50 +0200

    Test use of 'arm_right_rotate_to()'

--- robot_web_services/playground.py
+++ robot_web_services/playground.py
@@ -2,6 +2,7 @@
 
 import os
 import pathlib
+import time
 import sys
 import logging
 
@@ -32,14 +33,46 @@ def main() -> int:
         password=config["password"],
     )
 
-
     robot.task_1()
-    import time
     time.sleep(5)
     robot.task_2()
     robot.task_3()
-    robot.task_4()
 
+    # robot.arm_right_rotate_to(axis1=90,axis2=0,axis3=0,axis4=0,axis5=0,axis6=0)
+    robot.arm_right_rotate_to(axis1=91,axis2=5,axis3=-5,axis4=5,axis5=-5,axis6=10)
+
+    # print(robot._api_get(resource="/rw/motionsystem/mechunits/ROB_L/jointtarget"))
+    
+# [[-9.578368507,182.609892723,198.627808149],[0.066010726,0.842420918,-0.111214912,0.523068661],[0,0,0,4],[135,9E+09,9E+09,9E+09,9E+09,9E+09]]
+# [[0,-130,30,0,40,0],[135,9E+09,9E+09,9E+09,9E+09,9E+09]]
+
+    # Get winkel von arm links
+    # print(robot._api_get(resource="/rw/motionsystem/mechunits/ROB_L/jointtarget"))
+
+    # robot.task_1()
+    # import time
+    # time.sleep(5)
+    # robot.task_2()
+    # robot.task_3()
+    # robot.task_4()
+
+    # for index in range(100):
+    #     robot.move_arm_right(axis1=1,axis2=0,axis3=0,axis4=0,axis5=0,axis6=0,ccount=0,inc_mode="Large")
+    #     time.sleep(1)
+
+
+    # print(robot._api_get(resource="/rw/motionsystem/mechunits/ROB_L/axes/1?resource=axis-pose"))
+    # print(robot._api_get(resource="/rw/motionsystem/mechunits"))
+
+    # 90
+    # robot.move_arm_right(axis1=1,axis2=0,axis3=0,axis4=0,axis5=0,axis6=0,ccount=0,inc_mode="Large")
+    # # 90,14
+    
+    # time.sleep(5)
+
+    # # 90,14
+    # robot.move_arm_right(axis1=-1,axis2=0,axis3=0,axis4=0,axis5=0,axis6=0,ccount=0,inc_mode="Large")
+    # 98,86
 
     # robot.get_system()
 
