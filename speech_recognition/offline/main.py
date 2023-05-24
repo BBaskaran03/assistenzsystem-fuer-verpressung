1fda7744714648d22a041c60eb119606b11a2e12
Esat Sefa Bayhan <esat.bayhan@ruhr-uni-bochum.de>
2023-05-24 19:27:47 +0200

    Fix missing parameters

--- speech_recognition/offline/main.py
+++ speech_recognition/offline/main.py
@@ -2,18 +2,27 @@ from pvporcupine import create
 from pvrecorder import PvRecorder
 from keys import ACCESS_KEY
 from models.models import KEY_WORDS_PATH, MODEL_PATH, KEY_WORDS
+# from time import time
 
-recorder = PvRecorder()
-recorder.start()
 
 porcupine = create(
     access_key=ACCESS_KEY,
     keyword_paths=KEY_WORDS_PATH,
-    model_path=MODEL_PATH
+    model_path=MODEL_PATH,
+    sensitivities=[0.75, 0.75]
 )
 
+recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
+recorder.start()
+
 try:
+    # t = time()
+
     while True:
+        # if time() - t > 2:
+        #     t = time()
+        #     print("Still listening")
+
         pcm = recorder.read()
         keyword_index = porcupine.process(pcm)
 
