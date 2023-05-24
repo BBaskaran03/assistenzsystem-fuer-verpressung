35d9af78067d4887c85d09100276e945e2c36812
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-24 16:52:18 +0200

    Save and Reuse devcontainer image

---
+++ .devcontainer/documentation/image_build.ps1
@@ -0,0 +1,3 @@
+pushd ".devcontainer\documentation"
+docker build --tag "gitlab.ruhr-uni-bochum.de/menschenzentrierte-robotik/assistenzsystem-fuer-verpressung/devcontainer/documentation:latest" .
+popd
