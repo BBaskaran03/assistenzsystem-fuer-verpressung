55354aadb0ab2b7dc69b9bb4b1596c603bedf2a6
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-30 23:03:06 +0200

    Add templates for subtasks

--- main.py
+++ main.py
@@ -7,28 +7,48 @@ import speech_recognition
 import visual_detection
 
 
-def job(robot, speech, visual) -> int:
-    print("Hello, World!")
-
-    # // Optional
-    # - [Greifen] Yumi bewegt Arm-1 und greift das Gummiteil
+def job_grab_and_place_rubber(robot, speech, visual):
+    # - Yumi bewegt Arm-1 und greift das Gummiteil
     # - Yumi bewegt Arm-1 und "pudert" das Gummiteil
     # - Yumi bewegt Arm-1 und legt das Gummiteil in den Verpresser
     # - Yumi bewegt Arm-1 zurück in die Startposition
 
-    # // Must-Have
-    # - [Greifen] Yumi bewegt Arm-1 und greift das Metallteil
+    pass
+
+
+def job_grab_and_place_metal(robot, speech, visual):
+    # - Yumi bewegt Arm-1 und greift das Metallteil
     # - Yumi bewegt Arm-1 und legt das Metallteil in den Verpresser
     # - Yumi bewegt Arm-1 zurück in die Startposition
 
-    # // Must-Have
+    pass
+
+
+def job_move_tool_lever(robot, speech, visual):
     # - Yumi bewegt Arm-2 und hebt den Hebel hoch
     # - [Optional] Yumi bewegt Arm-2 und legt den Hebel um
     # - Yumi bewegt Arm-2 zurück in die Startposition
 
-    # // Must-Have
+    pass
+
+
+def job_grab_and_place_finished_product(robot, speech, visual):
     # - Yumi bewegt Arm-2 und legt das fertige Produkt in eine Box
     # - Yumi bewegt Arm-2 zurück in die Startposition
+
+    pass
+
+
+def job(robot, speech, visual) -> int:
+    print("Hello, World!")
+
+    running = True
+
+    while running:
+        job_grab_and_place_rubber()
+        job_grab_and_place_metal()
+        job_move_tool_lever()
+        job_grab_and_place_finished_product()
 
 
 def main() -> int:
