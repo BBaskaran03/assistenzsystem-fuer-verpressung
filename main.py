bc68bd0e35050cbbe54725dc100ba3a8ce48639d
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-30 22:42:45 +0200

    Simple template for integration

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import sys
 
 import robot_web_services
 import speech_recognition
+import visual_detection
+
+
+def job(robot, speech, visual) -> int:
+    print("Hello, World!")
 
 
 def main() -> int:
-    print("Hello, World!")
-    return 0
+    running = True
+
+    robot = robot_web_services.Robot()
+    speech = speech_recognition.Speech()
+    visual = visual_detection.Dectector()
+
+    return job(robot, speech, visual)
 
 
 if __name__ == "__main__":
