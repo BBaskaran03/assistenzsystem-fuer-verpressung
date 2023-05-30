c09b72096a68f71a3a4c370b192c3f19e9f57e26
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-30 23:19:03 +0200

    Dummy speech output

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ def job_grab_and_place_rubber(robot, speech, visual):
     # - Yumi bewegt Arm-1 und "pudert" das Gummiteil
     # - Yumi bewegt Arm-1 und legt das Gummiteil in den Verpresser
     # - Yumi bewegt Arm-1 zurück in die Startposition
+
+    speech.print("Ich greife jetzt das Gummiteil")
     
     robot.arm_left.move_to(position_box_rubber)
     position_rubber = visual.get_rubber()
@@ -44,6 +46,8 @@ def job_grab_and_place_metal(robot, speech, visual):
     # - Yumi bewegt Arm-1 und greift das Metallteil
     # - Yumi bewegt Arm-1 und legt das Metallteil in den Verpresser
     # - Yumi bewegt Arm-1 zurück in die Startposition
+
+    speech.print("Ich greife jetzt das Metallteil")
     
     robot.arm_left.move_to(position_box_metal)
     position_metal = visual.get_rubber()
@@ -62,6 +66,8 @@ def job_move_tool_lever(robot, speech, visual):
     # - [Optional] Yumi bewegt Arm-2 und legt den Hebel um
     # - Yumi bewegt Arm-2 zurück in die Startposition
 
+    speech.print("Ich lege jetzt den Hebel um")
+
     robot.arm_right.move_to(position_tool_lever)
 
     robot.arm_right.move_to(position_tool_lever_down)
@@ -77,9 +83,11 @@ def job_grab_and_place_finished_product(robot, speech, visual):
     # - Yumi bewegt Arm-2 und legt das fertige Produkt in eine Box
     # - Yumi bewegt Arm-2 zurück in die Startposition
 
+    speech.print("Ich greife jetzt das fertige Bauteil")
     robot.arm_right.move_to(position_tool_metal)
     robot.arm_right.grab(position_tool_metal)
 
+    speech.print("Ich lege jetzt das fertige Bauteil in die Box")
     robot.arm_right.move_to(position_box_finished)
     robot.arm_right.drop()
 
