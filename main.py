2e61bbbc5db51ac93b2a6483910bb7fb5322a733
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-05-30 22:45:35 +0200

    Add comments describing task

--- main.py
+++ main.py
@@ -10,6 +10,26 @@ import visual_detection
 def job(robot, speech, visual) -> int:
     print("Hello, World!")
 
+    # // Optional
+    # - [Greifen] Yumi bewegt Arm-1 und greift das Gummiteil
+    # - Yumi bewegt Arm-1 und "pudert" das Gummiteil
+    # - Yumi bewegt Arm-1 und legt das Gummiteil in den Verpresser
+    # - Yumi bewegt Arm-1 zurück in die Startposition
+
+    # // Must-Have
+    # - [Greifen] Yumi bewegt Arm-1 und greift das Metallteil
+    # - Yumi bewegt Arm-1 und legt das Metallteil in den Verpresser
+    # - Yumi bewegt Arm-1 zurück in die Startposition
+
+    # // Must-Have
+    # - Yumi bewegt Arm-2 und hebt den Hebel hoch
+    # - [Optional] Yumi bewegt Arm-2 und legt den Hebel um
+    # - Yumi bewegt Arm-2 zurück in die Startposition
+
+    # // Must-Have
+    # - Yumi bewegt Arm-2 und legt das fertige Produkt in eine Box
+    # - Yumi bewegt Arm-2 zurück in die Startposition
+
 
 def main() -> int:
     running = True
