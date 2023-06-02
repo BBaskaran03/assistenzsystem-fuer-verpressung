816e244ed0f32f94c2c446e3ff942cc948caf1af
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-06-02 16:29:26 +0200

    Add imports and demo to 'main.py'

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ import pathlib
 import sys
 
 import robot_web_services
+from robot_web_services.positions import Position
+from robot_web_services.positions import Positions
 import speech_recognition
 
 
+positions = None
+
+
 def main() -> int:
+    global positions
+    positions_file = "./positions.json"
+
     print("Hello, World!")
+
+    positions = Positions.from_file(positions_file)
+    positions["root"] = Position.from_worldpoint(0, 0, 0)
+    positions.to_file(positions_file)
+
     return 0
 
 
