c7b54b11bb16057330a9fe408214a8c6bbc1659c
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-06-02 21:04:18 +0200

    Cleanup: Formatting, Imports, Docstring

--- robot_web_services/robot_web_services.py
+++ robot_web_services/robot_web_services.py
@@ -1,77 +1,16 @@
-import sys
-import pathlib
-import ast
+"""Robot Web Services"""
+
 import enum
 import json
-import math
-import requests.auth
-import requests
-import time
 import logging
+import pathlib
+import sys
+import time
 
+import requests
+import requests.auth
 
 logger = logging.getLogger(__name__)
-
-
-def robtarget_to_position_target(robtarget) -> dict():
-    position_target = {}
-
-    position_target["pos-x"]=robtarget[0][(1-1)]
-    position_target["pos-y"]=robtarget[0][(2-1)]
-    position_target["pos-z"]=robtarget[0][(3-1)]
-    position_target["orient-q1"]=robtarget[1][(1-1)]
-    position_target["orient-q2"]=robtarget[1][(2-1)]
-    position_target["orient-q3"]=robtarget[1][(3-1)]
-    position_target["orient-q4"]=robtarget[1][(4-1)]
-    position_target["config-j1"]=robtarget[2][(1-1)]
-    position_target["config-j4"]=robtarget[2][(2-1)]
-    position_target["config-j6"]=robtarget[2][(3-1)]
-    position_target["config-jx"]=robtarget[2][(4-1)]
-    position_target["extjoint-1"]=robtarget[3][(1-1)]
-    position_target["extjoint-2"]=robtarget[3][(2-1)]
-    position_target["extjoint-3"]=robtarget[3][(3-1)]
-    position_target["extjoint-4"]=robtarget[3][(4-1)]
-    position_target["extjoint-5"]=robtarget[3][(5-1)]
-    position_target["extjoint-6"]=robtarget[3][(6-1)]
-
-    return position_target
-
-
-def position_target_to_payload(position_target):
-    payload = ""
-
-    payload = f'{payload}pos-x={position_target["pos-x"]}&'
-    payload = f'{payload}pos-y={position_target["pos-y"]}&'
-    payload = f'{payload}pos-z={position_target["pos-z"]}&'
-    payload = f'{payload}orient-q1={position_target["orient-q1"]}&'
-    payload = f'{payload}orient-q2={position_target["orient-q2"]}&'
-    payload = f'{payload}orient-q3={position_target["orient-q3"]}&'
-    payload = f'{payload}orient-q4={position_target["orient-q4"]}&'
-    payload = f'{payload}config-j1={position_target["config-j1"]}&'
-    payload = f'{payload}config-j4={position_target["config-j4"]}&'
-    payload = f'{payload}config-j6={position_target["config-j6"]}&'
-    payload = f'{payload}config-jx={position_target["config-jx"]}&'
-    payload = f'{payload}extjoint-1={position_target["extjoint-1"]}&'
-    payload = f'{payload}extjoint-2={position_target["extjoint-2"]}&'
-    payload = f'{payload}extjoint-3={position_target["extjoint-3"]}&'
-    payload = f'{payload}extjoint-4={position_target["extjoint-4"]}&'
-    payload = f'{payload}extjoint-5={position_target["extjoint-5"]}&'
-    payload = f'{payload}extjoint-6={position_target["extjoint-6"]}&'
-
-    payload = payload[:-1]
-
-    return payload
-
-
-def jointtarget_to_array(jointtarget):
-    axis1=jointtarget[0][(1-1)]
-    axis2=jointtarget[0][(2-1)]
-    axis3=jointtarget[0][(3-1)]
-    axis4=jointtarget[0][(4-1)]
-    axis5=jointtarget[0][(5-1)]
-    axis6=jointtarget[0][(6-1)]
-
-    return [axis1, axis2, axis3, axis4, axis5, axis6]
 
 
 class ControllerStates(enum.Enum):
@@ -90,13 +29,11 @@ class APIResponse:
         self._text = response.text
         self._json = self._maybe_json(response)
 
-
     def _maybe_json(self, response: requests.Response) -> json:
         try:
             return json.loads(response.text)
         except json.decoder.JSONDecodeError:
             return None
-
 
     @property
     def status_code(self) -> int:
@@ -114,24 +51,28 @@ class APIResponse:
         return json.dumps(self._json, indent=4)
 
 
-class RobotArm():
+class RobotArm:
     def __init__(self, robot, mechunit: str) -> None:
         self._robot = robot
         self._mechunit = mechunit
 
-
     def rotation_get(self):
-        response = self._robot._api_get(f"/rw/motionsystem/mechunits/{self._mechunit}/jointtarget")
+        response = self._robot._api_get(
+            f"/rw/motionsystem/mechunits/{self._mechunit}/jointtarget"
+        )
 
-        axis_get_value = lambda response, axis : response.json["_embedded"]["_state"][0][f"rax_{axis}"]
-        
+        axis_get_value = lambda response, axis: response.json["_embedded"]["_state"][0][
+            f"rax_{axis}"
+        ]
+
         axis_values = [axis_get_value(response, axis) for axis in range(1, (6 + 1))]
         axis_values = [float(value) for value in axis_values]
 
         return axis_values
 
-    
-    def _arm_job(self, axis1, axis2, axis3, axis4, axis5, axis6, ccount=0, inc_mode="Small"):
+    def _arm_job(
+        self, axis1, axis2, axis3, axis4, axis5, axis6, ccount=0, inc_mode="Small"
+    ):
         self._robot._api_post(f"/rw/motionsystem/{self._mechunit}") # TODO: Fix this
 
         payload = f"axis1={axis1}&axis2={axis2}&axis3={axis3}&axis4={axis4}&axis5={axis5}&axis6={axis6}"
@@ -139,22 +80,31 @@ class RobotArm():
 
         self._robot._api_post(resource="/rw/motionsystem?action=jog", payload=payload)
 
-
-    def rotation_set(self, axis1, axis2, axis3, axis4, axis5, axis6):        
+    def rotation_set(self, axis1, axis2, axis3, axis4, axis5, axis6):
         axis_target = [axis1, axis2, axis3, axis4, axis5, axis6]
         axis_target = [int(value) for value in axis_target]
 
-        evaluate = lambda target, value : (+1) if (value < target) else (-1) if (value > target) else (0)
+        evaluate = (
+            lambda target, value: (+1)
+            if (value < target)
+            else (-1)
+            if (value > target)
+            else (0)
+        )
 
         while True:
             axis_current = self.rotation_get()
             axis_current = [int(value) for value in axis_current]
-            
-            if (axis_target == axis_current): break
 
-            movement = [evaluate(target, value) for target, value in zip(axis_target, axis_current)]
+            if axis_target == axis_current:
+                break
+
+            movement = [
+                evaluate(target, value)
+                for target, value in zip(axis_target, axis_current)
+            ]
             self._arm_job(*movement, 0, "Large")
-            
+
             time.sleep(1)
 
 
@@ -167,9 +117,8 @@ class RobotWebServices:
 
     headers = {
         "Accept": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded;v=2.0"
+        "Content-Type": "application/x-www-form-urlencoded;v=2.0",
     }
-
 
     def __init__(self, hostname: str, username: str, password: str, model: str):
         self.hostname = hostname
@@ -180,15 +129,13 @@ class RobotWebServices:
         self.session = requests.Session()
         self.session.auth = requests.auth.HTTPDigestAuth(username, password)
 
-
     def _adapt_to_model(self):
         if self.model == "IRB14000":
             self.arm_left = RobotArm(self, "ROB_L")
             self.arm_right = RobotArm(self, "ROB_R")
             return
-        
-        raise Exception("Unknown model")
 
+        raise Exception("Unknown model")
 
     def _api_get(self, resource) -> APIResponse:
         url = f"{self.hostname}/{resource}"
@@ -198,7 +145,7 @@ class RobotWebServices:
             url=url,
             headers=self.headers,
             cookies=self.session.cookies,
-            auth=self.session.auth
+            auth=self.session.auth,
         )
 
         response = APIResponse(response)
@@ -209,8 +156,7 @@ class RobotWebServices:
 
         return response
 
-
-    def _api_post(self, resource, payload = None) -> APIResponse:
+    def _api_post(self, resource, payload=None) -> APIResponse:
         url = f"{self.hostname}/{resource}"
         url = f"{url}&json=1" if "?" in url else f"{url}?json=1"
 
@@ -219,7 +165,7 @@ class RobotWebServices:
             data=payload,
             headers=self.headers,
             cookies=self.session.cookies,
-            auth=self.session.auth
+            auth=self.session.auth,
         )
 
         response = APIResponse(repsonse)
@@ -230,25 +176,20 @@ class RobotWebServices:
 
         return response
 
-
     def login(self):
         self._api_post(resource="/users?action=set-locale", payload="type=local")
 
     def rmmp(self):
-        self._api_post(resource="/users/rmmp",payload="privilege=modify")
-
+        self._api_post(resource="/users/rmmp", payload="privilege=modify")
 
     def get_system(self):
         self._api_get("/rw/system?json=1")
 
-
     def request_mastership(self):
         self._api_post(resource="/rw/mastership?action=request")
 
-
     def release_mastership(self):
         self._api_post("/rw/mastership?action=release")
-
 
     def ready_robot(self):
         self.login()
@@ -257,7 +198,6 @@ class RobotWebServices:
 
         self.rmmp()
         self.request_mastership()
-
 
     def get_controller_state(self) -> ControllerStates:
         """
@@ -269,11 +209,10 @@ class RobotWebServices:
         """
 
         response = self._api_get("rw/panel/ctrlstate")
-        state = response.json["_embedded"]["_state"][0]['ctrlstate']
+        state = response.json["_embedded"]["_state"][0]["ctrlstate"]
         state = ControllerStates[state]
 
         return state
-
 
     def set_controller_state(self, ctrl_state) -> APIResponse:
         payload = {"ctrl-state": ctrl_state}
@@ -287,7 +226,6 @@ class RobotWebServices:
 
         return response
 
-
     def motors_on(self) -> None:
         """Turns the robot's motors on.
         Operation mode has to be AUTO.
@@ -295,12 +233,10 @@ class RobotWebServices:
 
         self.set_controller_state(ControllerStates.motoron.value)
 
-
     def motors_off(self):
-        """Turns the robot's motors off.
-        """
+        """Turns the robot's motors off."""
 
-        payload = {'ctrl-state': ControllerStates.motoroff}
+        payload = {"ctrl-state": ControllerStates.motoroff}
         self._api_post("/rw/panel/ctrlstate?action=setctrlstate", payload)
 
 
