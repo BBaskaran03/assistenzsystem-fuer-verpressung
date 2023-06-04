c066db9b87fba4fff6f0a349006854b329304faa
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-06-04 18:36:08 +0200

    Add method 'to_array()' for class 'Position'

--- robot_web_services/positions.py
+++ robot_web_services/positions.py
@@ -276,6 +276,16 @@ class Position(Jointtarget, Robtarget):
 
         return json
 
+    def to_array(self) -> list[list[int]]:
+        array = [
+            self.worldpoint.to_array(),
+            self.orientation.to_array(),
+            self.axis_configuration.to_array(),
+            self.external_axes_position.to_array()
+        ]
+
+        return array
+
     def __str__(self):
         return str(self.json)
 
