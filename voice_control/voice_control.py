116e9052b00e048c22ead95a37051539a3909656
Esat Sefa Bayhan <esat.bayhan@ruhr-uni-bochum.de>
2023-06-06 14:50:36 +0200

    Added context recognition

--- voice_control/voice_control.py
+++ voice_control/voice_control.py
@@ -1,11 +1,18 @@
 import sys
+from chat import Chat
+from hotword import Hotword
 
 
 class VoiceControl():
-    def __init__(self): pass
+    def __init__(self):
+        self.chat = Chat()
 
     def listen(): pass
 
+    def check_context_stop(self, context: str) -> bool:
+        if context.tolower().startswith("stop"):
+            # Todo implement here signal
+            return True
 
 
 def main() -> int:
