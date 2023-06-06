e22fee8321baf0fa7d7f394961e05c46cae64a69
Banujan Baskaran <banujan.baskaran@ruhr-uni-bochum.de>
2023-06-06 16:13:49 +0200

    haher

--- bildverarbeitung/capture.py
+++ bildverarbeitung/capture.py
@@ -1,11 +1,20 @@
 import cv2 as cv
-
-cam = cv.VideoCapture(0)
-
+cam_port = 0
+cam = cv.VideoCapture(cam_port, cv.CAP_DSHOW)
+#cam.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+#cam.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+#cam.set(cv.CAP_PROP_EXPOSURE, 8)
+#cam.set(cv.CAP_PROP_BACKLIGHT, 8)
+  
+# reading the input using the camera
 result, image = cam.read()
+  
 
-cv.imshow("HI", image)
-cv.imwrite("image.png", image)
-
-cv.waitKey(0)
-cv.destroyWindow("HI")
+if result:
+    
+    # saving image in local storage
+    cv.imwrite("image.jpg", image)
+  
+# If captured image is corrupted, moving to else part
+else:
+    print("No image detected. Please! try again")
