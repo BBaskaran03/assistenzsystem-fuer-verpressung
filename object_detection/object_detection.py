490f4b367623eda883464b707e9490358efca627
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-06-08 20:10:58 +0200

    Add template for conversion of visual analysis to robot position

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -1,17 +1,45 @@
 import sys
 
-from robot_web_services.positions import Position
+from robot_web_services.positions import Position, Robtarget
 
 
-class ObjectDetector():
-    def __init__(self): pass
+class GrabTarget:
+    def __init__(self, position: Position, rotation: float):
+        self.position = position
+        self.rotation = rotation
+
+    def get_grab_position() -> Position:
+        # TODO: Calulate robtarget from self.position
+        return Position.from_robtarget(None)
+
+
+class ObjectDetector:
+    def __init__(self):
+        pass
 
     def get_rubber(self) -> Position:
-        return Position.from_worldpoint(x=0, y=0, z=0)
+        # Example values for location of object
+        position_pixel = {"x": 960, "y": 540}
+        rotation = 180.0
 
+        position = Position.from_worldpoint(
+            x=position_pixel["x"], y=position_pixel["y"], z=0
+        )
+
+        grab_target = GrabTarget(position, rotation)
+        return grab_target.get_grab_position()
 
     def get_metal(self) -> Position:
-        return Position.from_worldpoint(x=0, y=0, z=0)
+        # Example values for location of object
+        position_pixel = {"x": 960, "y": 540}
+        rotation = 180.0
+
+        position = Position.from_worldpoint(
+            x=position_pixel["x"], y=position_pixel["y"], z=0
+        )
+
+        grab_target = GrabTarget(position, rotation)
+        return grab_target.get_grab_position()
 
 
 def main() -> int:
