e61d4a4f19ccd0d7e9fdc48c93756273f6ef5f13
Simon Spettmann <simon.spettmann@ruhr-uni-bochum.de>
2023-06-08 16:43:15 +0200

    Select 'PORCUPINE' models depending on platform

--- voice_control/models/models.py
+++ voice_control/models/models.py
@@ -1,18 +1,14 @@
+import platform
 from os import path
 
-__file_path = path.realpath(__file__)
-__dir_path = path.dirname(__file_path)
+this_platform = platform.system().lower()
+directory = path.dirname(path.realpath(__file__))
 
+HEY_YUMI = path.join(directory, f"Hey-Yu-Mi_de_{this_platform}_v2_2_0.ppn")
+YUMI_STOP = path.join(directory, f"Yu-Mi-Stop_de_{this_platform}_v2_2_0.ppn")
+YUMI_WEITER = path.join(directory, f"Yu-Mi-Weiter_de_{this_platform}_v2_2_0.ppn")
 
-__YUMI_STOP = path.join(__dir_path, "Yu-Mi-Stop_de_linux_v2_2_0.ppn")
-__YUMI_WEITER = path.join(__dir_path, "Yu-Mi-Weiter_de_linux_v2_2_0.ppn")
-__HEY_YUMI = path.join(__dir_path, "Hey-Yu-Mi_de_linux_v2_2_0.ppn")
+KEY_WORDS = ["HEY_YUMI", "YUMI_STOP", "YUMI_WEITER"]
+KEY_WORDS_PATH = [HEY_YUMI, YUMI_STOP, YUMI_WEITER]
 
-MODEL_PATH = path.join(__dir_path, "porcupine_params_de.pv")
-
-KEY_WORDS_PATH = [__YUMI_STOP, __YUMI_WEITER, __HEY_YUMI]
-KEY_WORDS = [
-    "YUMI_STOP",
-    "YUMI_WEITER",
-    "HEY_YUMI"
-]
+MODEL_PATH = path.join(directory, "porcupine_params_de.pv")
