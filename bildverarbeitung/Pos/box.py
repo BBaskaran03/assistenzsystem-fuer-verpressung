ba62d3fdfd84d687c3e88f038d03287dd8724260
christopher kamphausen <christopher.kamphausen@rub.de>
2023-06-08 14:29:00 +0200

    Erkennt Objekt mit der goessten kante und gibt Orientierung und Mittelpunkt aus

--- bildverarbeitung/Pos/box.py
+++ bildverarbeitung/Pos/box.py
@@ -1,92 +1,145 @@
-import cv2
+import cv2 as cv
+from math import atan2, cos, sin, sqrt, pi
+import numpy as np
+ 
+def adjust_brightness_contrast(image, brightness, contrast):
+    adjusted_image = cv.convertScaleAbs(image, alpha=contrast, beta=brightness)
+    return adjusted_image
 
-# Funktion zum Zeichnen einer Box um die Kanten
-def draw_box(image, bbox, color=(0, 255, 0), thickness=4):
-    x, y, w, h = bbox
-    cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
+def drawAxis(img, p_, q_, color, scale):
+  p = list(p_)
+  q = list(q_)
+ 
+  ## [visualization1]
+  angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+  hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+ 
+  # Here we lengthen the arrow by a factor of scale
+  q[0] = p[0] - scale * hypotenuse * cos(angle)
+  q[1] = p[1] - scale * hypotenuse * sin(angle)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+ 
+  # create the arrow hooks
+  p[0] = q[0] + 9 * cos(angle + pi / 4)
+  p[1] = q[1] + 9 * sin(angle + pi / 4)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+ 
+  p[0] = q[0] + 9 * cos(angle - pi / 4)
+  p[1] = q[1] + 9 * sin(angle - pi / 4)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+  ## [visualization1]
+ 
+def getOrientation(pts, img):
+  ## [pca]
+  # Construct a buffer used by the pca analysis
+  sz = len(pts)
+  data_pts = np.empty((sz, 2), dtype=np.float64)
+  for i in range(data_pts.shape[0]):
+    data_pts[i,0] = pts[i,0,0]
+    data_pts[i,1] = pts[i,0,1]
+ 
+  # Perform PCA analysis
+  mean = np.empty((0))
+  mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
+ 
+  # Store the center of the object
+  cntr = (int(mean[0,0]), int(mean[0,1]))
+  ## [pca]
+ 
+  ## [visualization]
+  # Draw the principal components
+  cv.circle(img, cntr, 3, (255, 0, 255), 2)
+  p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+  p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+  drawAxis(img, cntr, p1, (255, 255, 0), 0.25)
+  drawAxis(img, cntr, p2, (0, 0, 255), 2)
+ 
+  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+  ## [visualization]
+ 
+  # Label with the rotation angle
+  label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
+  textbox = cv.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+  cv.putText(img, label, (cntr[0], cntr[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv.LINE_AA)
+ 
+  return angle, cntr
+ 
+def main(image):
+    print(image)
+    # Load the image
+    img = cv.imread(image)
 
-# Funktion zum Zeichnen des Mittelpunkts
-def draw_center(image, center, color=(0, 0, 255), radius=5):
-    cv2.circle(image, center, radius, color, 20)
+    # Was the image there?
+    if img is None:
+        print("Error: File not found")
+        exit(0)
 
-# Funktion zum Zeichnen der Achse
-def draw_axis(image, point1, point2, color=(255, 0, 0), thickness=3):
-    cv2.line(image, point1, point2, color, thickness)
+    # Convert image to grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = adjust_brightness_contrast(gray, -500, 5)
 
-# Funktion zur Berechnung des Mittelpunkts und Zeichnen der Box und Achse
-def mittelpunkt(image_path):
-    print(image_path)
 
-    # Laden des Bildes
-    image = cv2.imread(image_path)
+    blurred = cv.blur(gray, (10, 10), 0)
+    thresh = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 19, 1)
 
-    # Konvertieren in Graustufenbild
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Anwenden der Canny-Kantenerkennung
-    edges = cv2.Canny(gray, 20, 150)
-    cv2.imwrite("edge.jpg", edges)
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    gray = clahe.apply(thresh)
 
-    # Suchen der Konturen im Bild
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv.imwrite("gray.jpg", gray)
 
-    # Ermitteln der äußersten Koordinaten aller Boxen, die eine Mindestgröße überschreiten
-    min_size = 100  # Mindestgröße für eine Box
-    x_coords = []
-    y_coords = []
-    widths = []
-    heights = []
+    # Convert image to binary
+    bw = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+
+    # Find all the contours in the thresholded image
+    contours, _ = cv.findContours(bw, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+    merged_contours = []
     for contour in contours:
-        # Berechnen der Begrenzungsbox um die Kontur
-        x, y, w, h = cv2.boundingRect(contour)
-        if w > min_size and h > min_size:
-            x_coords.append(x)
-            y_coords.append(y)
-            widths.append(w)
-            heights.append(h)
+        # Calculate the area of each contour
+        area = cv.contourArea(contour)
 
-    # Ermitteln der größten umfassenden Box
-    if x_coords and y_coords and widths and heights:
-        x_min = min(x_coords)
-        y_min = min(y_coords)
-        x_max = max(x_coords) + max(widths)
-        y_max = max(y_coords) + max(heights)
-        bbox = (x_min, y_min, x_max - x_min, y_max - y_min)
+        # Ignore contours that are too small or too large
+        if area < 50000 or 150000 < area:
+            continue
 
-        # Zeichnen der umfassenden Box
-        draw_box(image, bbox)
+        # Approximate the contour with convex hull
+        hull = cv.convexHull(contour)
 
-        # Ermitteln der Mittelpunkt der Box
-        center = (x_min + int((x_max - x_min) / 2), y_min + int((y_max - y_min) / 2))
+        # Add the approximated contour to the merged contours list
+        merged_contours.append(hull)
 
-        # Zeichnen des Mittelpunkts in Rot
-        draw_center(image, center)
+    # Draw largest of merged contours
 
-        # Ermitteln der Längsachse
-        if x_max - x_min > y_max - y_min:
-            # Längsachse verläuft horizontal
-            point1 = (x_min, center[1])
-            point2 = (x_max, center[1])
-            print("horizontal")
-        else:
-            # Längsachse verläuft vertikal
-            point1 = (center[0], y_min)
-            point2 = (center[0], y_max)
-            print("vertikal")
+    merged_contours = sorted(merged_contours, key=cv.contourArea, reverse=True)
 
-        # Zeichnen der Längsachse in Blau
-        draw_axis(image, point1, point2)
-
-    # Speichern des Bildes mit der gezeichneten Box und Achse
-    cv2.imwrite('box15.jpg', image)
     try:
-        print(center)   
-        print("\n")
+        largest_contour = merged_contours[0]
+        cv.drawContours(img, [largest_contour], -1, (0, 0, 255), 2)
+
+        #print(-int(np.rad2deg(getOrientation(largest_contour, img))) - 90)
+
+        angle, center  = getOrientation(largest_contour, img)
+        print("Rotation Angle:", -int(np.rad2deg(angle)) - 90)
+        print("Center:", center)
+        # Save the output image to the current directory
+        cv.imwrite("out_"+image, img)
+
     except:
-        print("Error no center value")
+       print("Error please reposition the coponent") 
+
+for i in range(1,44):
+   
+   main("Rohr ("+ str(i)+").jpg")
 
 
-# Aufruf der Funktion mit dem Pfad zum Bild
+"""
+Variablen zum rumspielen:
+Z.103 (area<50000)
 
 
-mittelpunkt('rohr (14).jpg')
+
+
+
+
+"""
